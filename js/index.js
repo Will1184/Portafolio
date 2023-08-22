ed917a8864f231e434f93a7a7ee330139811e5f8
Will1184 <brandonwilliammg@gmail.com>
2023-08-22 17:12:58 -0600

    Updates

--- js/index.js
+++ js/index.js
@@ -1,25 +1,171 @@
-// Creacion de un scroll behavior
-$(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
+const home = document.getElementById('home');
+const cv = document.getElementById('cv');
+const springUniversidad= document.getElementById('spring-universidad');
+const loginCrudReact = document.getElementById('login-crud-react');
+const loginCrudSpring = document.getElementById('login-crud-spring');
+const loginCrudJava = document.getElementById('login-crud-java');
+const loginFirebaseAndroid = document.getElementById('login-firebase-android');
+const adminPacientesReact = document.getElementById('admin-pacientes-react');
+
+const mostrarBtnCv = document.getElementById('btn-cv');
+const mostrarBtnSpringUniversidad = document.getElementById('btn-spring-universidad');
+const mostrarBtnSpringCrud = document.getElementById('btn-login-crud-spring');
+const mostrarBtnReactCrud = document.getElementById('btn-login-crud-react');
+const mostrarBtnAdminPacientesReact = document.getElementById('btn-login-crud-java');
+const mostrarBtnJavaCrud = document.getElementById('btn-login-firebase-android');
+const mostrarBtnLoginFirebase = document.getElementById('btn-admin-pacientes-react');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const footer = document.getElementById('mi-footer');  
+  const currentTime = new Date();
+  const anio = currentTime.getFullYear()  
+  footer.textContent = `Brandon Gomez ${anio}`
+});
+
+
+mostrarBtnCv.addEventListener('click', () => { 
+  hideAll();
+  const result = verificarBtn(cv);
+  if(result){
+    hide(cv);
+    show(home);
+  }else{
+    show(cv);
+  }
+});
+
+mostrarBtnSpringUniversidad.addEventListener('click', () => {
+  hideAll();
+  const result = verificarBtn(springUniversidad);
+  if(result){
+    hide(springUniversidad);
+    show(home);
+  }else{
+    show(springUniversidad);
+  }
+});
+
+mostrarBtnReactCrud.addEventListener('click', () => {
+  hideAll();
+  const result = verificarBtn(loginCrudReact);
+  if(result){
+    hide(loginCrudReact);
+    show(home);
+  }else{
+    show(loginCrudReact);
+  }
+});
+
+mostrarBtnJavaCrud.addEventListener('click', () => {
+  hideAll();
+  const result = verificarBtn(loginCrudJava);
+  if(result){
+    hide(loginCrudJava);
+    show(home);
+  }else{
+    show(loginCrudJava);
+  }
+});
+
+mostrarBtnSpringCrud.addEventListener('click', () => {
+  hideAll();
+  const result = verificarBtn(loginCrudSpring);
+  if(result){
+    hide(loginCrudSpring);
+    show(home);
+  }else{
+    show(loginCrudSpring);
+  }
+});
+
+mostrarBtnLoginFirebase.addEventListener('click', () => {
+  hideAll();
+  const result = verificarBtn(loginFirebaseAndroid);
+  if(result){
+    hide(loginFirebaseAndroid);
+    show(home);
+  }else{
+    show(loginFirebaseAndroid);
+  }
+});
+
+mostrarBtnAdminPacientesReact.addEventListener('click', () => {
+  hideAll();
+  const result = verificarBtn(adminPacientesReact);
+  if(result){
+    hide(adminPacientesReact);
+    show(home);
+  }else{
+    show(adminPacientesReact);
+  }
+});
+
+const hideAll=()=>{    
+  let result;
   
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
+  result=verificarBtn(home)
+  if(result){
+    hide(home);    
+  }
+
+  result=verificarBtn(cv)
+  if(result){
+    hide(cv);    
+  }
   
-        // Store hash
-        var hash = this.hash;
+  result=verificarBtn(springUniversidad)
+  if(result){
+    hide(springUniversidad);    
+  }
   
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
+  result=verificarBtn(loginCrudSpring)
+  if(result){
+    hide(loginCrudSpring);    
+  }
   
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
-  });
+  result=verificarBtn(loginCrudReact)
+  if(result){
+    hide(loginCrudReact);    
+  }
+  
+  result=verificarBtn(loginCrudJava)
+  if(result){
+    hide(loginCrudJava);    
+  }
+  
+  result=verificarBtn(loginFirebaseAndroid)
+  if(result){
+    hide(loginFirebaseAndroid);    
+  }
+  
+  result=verificarBtn(adminPacientesReact)
+  if(result){
+    hide(adminPacientesReact);    
+  }
+}
+
+const hide = (nameDiv)=>{  
+  nameDiv.classList.add('-hide');    
+  nameDiv.classList.remove('-show');
+  setTimeout(() => {
+    nameDiv.style.display="none"
+    }, 900);    
+}
+
+const show = (nameDiv)=>{
+  setTimeout(() => {
+    nameDiv.style.display='block';
+    nameDiv.classList.remove('-hide');    
+    nameDiv.classList.add('-show')
+  }, 1000);    
+}
+
+const verificarBtn = (div) => {
+  const estiloDisplay = window.getComputedStyle(div).getPropertyValue('display');
+  const result = estiloDisplay === "block";
+  return result;
+};
+
+
+
+
